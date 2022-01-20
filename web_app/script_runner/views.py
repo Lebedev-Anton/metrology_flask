e86ffefdbb5da79f872e279489990cc1a154fd73
Anton Lebedev <anton-lebedev-87@mail.ru>
2022-01-20 18:57:57 +0300

    Исправление сериализации

--- web_app/script_runner/views.py
+++ web_app/script_runner/views.py
@@ -20,7 +20,7 @@ def start_script():
             Devices.order_number == form.order_number.data).first()[0]
         script_path_for_import = Scripts.query.filter_by(script_name=form.script.data).first().path
         user_id = current_user.id
-        celery_task = run_checked_point_in_celery.delay(work_id, script_path_for_import, user_id)
+        celery_task = start_script_in_celery.delay(work_id, script_path_for_import, user_id)
         return redirect(url_for('script_runner.task_status', task_id=celery_task.id))
     return render_template('index.html', form=form)
 
@@ -30,21 +30,28 @@ def run_script(checked_point_id, path):
     work_id = db.session.query(CheckedPoint.id_work).join(CheckedPointData).filter(
         CheckedPointData.id_checked_point == checked_point_id).first()[0]
     user_id = current_user.id
-    return run_checked_point(user_id, work_id, path)
+    celery_task = run_script_in_celery.delay(work_id, path, user_id)
+    return redirect(url_for('script_runner.task_status', task_id=celery_task.id))
 
 
 @blueprint.route('/task_status/<task_id>', methods=['GET', 'POST'])
 def task_status(task_id):
-    task = run_checked_point_in_celery.AsyncResult(task_id)
+    task = start_script_in_celery.AsyncResult(task_id)
     if task.state == 'PENDING':
         return "Задача выполняется. Ожидайте"
     elif task.state != 'FAILURE':
         return task.result
-    return str(task.info) + 'ОШИБКА ' + task.state
+    return str(task.info)
 
 
-@celery_app.task(serializer='json')
-def run_checked_point_in_celery(work_id, script_path_for_import, user_id):
+@celery_app.task
+def start_script_in_celery(work_id, script_path_for_import, user_id):
     with app.app_context(), app.test_request_context():
         load_checked_point_in_db(script_path_for_import, work_id)
+        return run_checked_point(user_id, work_id, script_path_for_import)
+
+
+@celery_app.task
+def run_script_in_celery(work_id, script_path_for_import, user_id):
+    with app.app_context(), app.test_request_context():
         return run_checked_point(user_id, work_id, script_path_for_import)
