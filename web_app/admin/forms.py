db02145a1a252e0cf7083a99c387d47a106d2dbc
Anton Lebedev <anton-lebedev-87@mail.ru>
2022-01-28 23:31:08 +0300

    Перенос запрсосов в бд в lambda функции

--- web_app/admin/forms.py
+++ web_app/admin/forms.py
@@ -10,18 +10,30 @@ from web_app.user.user_validators import \
     validate_email, validate_username, validate_password, validate_employee_position, validate_responsible_user
 
 
-class EditUserFrom(FlaskForm):
-    username = StringField('Введите имя пользователя', validators=[DataRequired()])
+def get_users_from_db():
+    users_from_db = db.session.query(Users.username).all()
+    users = [user[0] for user in users_from_db]
+    return users
+
+
+def get_work_types_from_db():
+    work_types_from_db = db.session.query(WorkType.work_type_name).all()
+    allowed_work_types = [work_type[0] for work_type in work_types_from_db]
+    return allowed_work_types
+
+
+def get_allowed_position():
     positions_from_db = db.session.query(PositionsEmployees.position_name).all()
     allowed_position = [position[0] for position in positions_from_db]
-    employee_position = SelectField('Введите должность', validators=[DataRequired(), validate_employee_position],
-                                    choices=allowed_position)
+    return allowed_position
 
-    work_types_from_db = db.session.query(WorkType.work_type_name).all()
-    work_types = set()
-    for work_type in work_types_from_db:
-        work_types.add(str(work_type[0]))
-    allowed_work_types = SelectMultipleField('Выберете допустимые виды работ', choices=work_types)
+
+class EditUserFrom(FlaskForm):
+    username = StringField('Введите имя пользователя', validators=[DataRequired()])
+
+    employee_position = SelectField('Введите должность', validators=[DataRequired(), validate_employee_position],
+                                    choices=lambda: get_allowed_position())
+    allowed_work_types = SelectMultipleField('Выберете допустимые виды работ', choices=lambda: get_work_types_from_db())
 
     email = EmailField('Введите email', validators=[DataRequired(), validate_email])
 
@@ -38,18 +50,14 @@ class DevicesForm(FlaskForm):
     modification = StringField('Введите модификацию', validators=[DataRequired()])
     delivery_date = DateField('Введите дату поступления', validators=[DataRequired()])
 
-    work_types_from_db = db.session.query(WorkType.work_type_name).all()
-    allowed_work_types = [work_type[0] for work_type in work_types_from_db]
     work_type = SelectField('Выберете вид работ', validators=[DataRequired()],
-                            choices=allowed_work_types)
+                            choices=lambda: get_work_types_from_db())
 
 
 class WorkStatusForm(FlaskForm):
     order_number = StringField('Введите номер заявки', validators=[DataRequired()])
-
-    users_from_db = db.session.query(Users.username).all()
-    users = [user[0] for user in users_from_db]
-    user = SelectField('Выберете исполнителя', validators=[DataRequired(), validate_responsible_user], choices=users)
+    user = SelectField('Выберете исполнителя', validators=[DataRequired(), validate_responsible_user],
+                       choices=lambda: get_users_from_db())
 
     work_status = SelectField('Выберете статут работ', validators=[DataRequired()],
                               choices=['backlog', 'progress', 'done'])
